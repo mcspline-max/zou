0cec0495f19d7ac369c080f25bf6c7c8972dbc12
Mathieu <mathieu.clv@gmail.com>
2026-08-23 18:01:25 +0200

    Faster previews conversion - FFmpeg switch to Ultra-Fast preset

--- zou/utils/movie.py
+++ zou/utils/movie.py
@@ -230,7 +230,7 @@ def normalize_encoding(
         format="mp4",
         r=fps,
         b=b,
-        preset="fast",
+        preset="ultrafast",
         vcodec="libx264",
         movflags="+faststart",
         x264opts=f"keyint={keyframes}:scenecut=0",
